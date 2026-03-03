19990057127de3f2a65e54d0681216767bd69806
Tekbuildz <tekbuildz@gmail.com>
2026-03-03 16:14:03 +0100

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
     content: [
         // Note: The following list of file extensions must NOT contain whitespaces, as e.g. {html, ts} will
         // search for '*.html' and '*. ts' - the latter not yielding any results
-        './chrome/**/*.{html,js,ts}',
+        "./chrome/background.{ts,js}",
+        "./chrome/proxy-help.html",
+        "./chrome/background_helpers/**/*.{html,js,ts}",
+        "./chrome/checking/**/*.{html,js,ts}",
+        "./chrome/options/**/*.{html,js,ts}",
+        "./chrome/popup/**/*.{html,js,ts}",
+        "./chrome/shared/**/*.{html,js,ts}",
     ],
     theme: {
         extend: {},
