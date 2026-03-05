9532f1799de34a83dedff3132b3ab6192861934e
Tekbuildz <tekbuildz@gmail.com>
2026-03-05 10:32:46 +0100

    removed old options HTML and TS

--- chrome/src/options/options_initializer.ts
+++
@@ -1,6 +0,0 @@
-// initialization logic for values used by all sections/tabs
-import {initializeStrictModes} from "../shared/utilities.js";
-
-document.addEventListener('DOMContentLoaded', async () => {
-    await initializeStrictModes();
-});
